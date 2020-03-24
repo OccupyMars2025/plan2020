8f6857bce6428dbe2d29959fe7aacc7bd692152a
Kol <31559413+SmirnovKol@users.noreply.github.com>
2020-03-25 06:40:52 +0800

    Update plan202003.py

--- plan202003.py
+++ plan202003.py
@@ -1,11 +1,24 @@
 """
 2020/3/24-3/29: 
+24, 25, 26, 27, 28, 29
 1.Python Machine Learning 2nd Edition
+2.百面机器学习
 
-2020/3/24: 
+
+
+
+2020/3/24, Tuesday:  第 1 天
 1.高抬腿 600 个， pull-up 300, push-up 500, 臂力器 500 
-实际完成：高抬腿 300 个， pull-up 100, push-up 0 , 臂力器 0 
+实际完成：高抬腿 600 个， pull-up 300, push-up 40 , 臂力器 40
 2.Python Machine Learning 2nd Edition, page 71-150 
-morning: page 71-90
+实际完成: page 71-87
+
+
+2020/3/25, Wednesday:  第 2 天
+1.高抬腿 600 个， pull-up 300, push-up 960, 臂力器 960 
+实际完成：
+2.Python Machine Learning 2nd Edition, page 87-250
+实际完成:
+3.百面机器学习， page 14-80
 实际完成:
 """
