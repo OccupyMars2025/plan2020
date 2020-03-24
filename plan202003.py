feb8b85f9e453a12d3ee57e90dad23e424b364d1
Kol <31559413+SmirnovKol@users.noreply.github.com>
2020-03-24 08:58:47 +0800

    Create plan202003.py

---
+++ plan202003.py
@@ -0,0 +1,11 @@
+"""
+2020/3/24-3/29: 
+1.Python Machine Learning 2nd Edition
+
+2020/3/24: 
+1.高抬腿 600 个， pull-up 300, push-up 500, 臂力器 500 
+实际完成：高抬腿 300 个， pull-up 100, push-up 0 , 臂力器 0 
+2.Python Machine Learning 2nd Edition, page 71-150 
+morning: page 71-90
+实际完成:
+"""
